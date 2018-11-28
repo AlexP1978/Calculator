5c3b05eee5dbdf3b9d6524d4cbbb62c1d35e9322
AlexP1978 <ttt333@ukr.net>
2018-11-28 11:51:05 +0200

    adding files

---
+++ Calculator/Classses/Calculate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Classses
+{
+    public class Calculate
+    {
+        //Add delegate
+        private delegate double OperationDelegate(double x, double y);
+
+        private Dictionary<string, Func<double, double, double>> _operations =
+            new Dictionary<string, Func<double, double, double>>
+            {
+        { "+", (x, y) => x + y },
+        { "-", (x, y) => x - y },
+        { "*", (x, y) => x * y },
+        { "/", (x, y) => x / y },
+            };
+
+        // processing operation
+        public double PerformOperation(string op, double x, double y)
+        {
+            if (!_operations.ContainsKey(op))
+                throw new ArgumentException(string.Format("Operation {0} is invalid", op), "op");
+            return _operations[op](x, y);
+        }
+        // new operation
+        public void DefineOperation(string op, Func<double, double, double> body)
+        {
+            if (_operations.ContainsKey(op))
+                throw new ArgumentException(string.Format("Operation {0} already exists", op), "op");
+            _operations.Add(op, body);
+        }
+
+    }
+}
--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculator.Classses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Calculator
 {
     /// <summary>
@@ -20,9 +22,88 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+            string leftop = ""; // left operand
+            string operation = ""; // sign of operation
+            string rightop = ""; // right operand
+
+
         public MainWindow()
         {
+
             InitializeComponent();
+
+            // Add handler for all buttons on the grid
+            foreach (UIElement c in LayoutRoot.Children)
+            {
+                if (c is Button)
+                {
+                    ((Button)c).Click += Button_Click;
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // get the button text
+            string s = (string)((Button)e.OriginalSource).Content;
+            // add it in textbox
+            textBox.Text += s;
+
+            int num;
+            // trying convert it to number
+            bool result = Int32.TryParse(s, out num);
+
+            // if text is number
+            if (result == true)
+            {
+                if (operation == "")
+                {
+                    leftop += s;
+                }
+                else
+                {
+                    rightop += s;
+                }
+            }
+            // if text is not number
+            else
+            {
+                if (s == "=")
+                {
+                    Update_RightOp();
+                    //textBox.Text += rightop;
+                    textBox.Text = rightop;
+                    operation = "";
+                }
+                // clear field
+                else if (s == "C")
+                {
+                    leftop = "";
+                    rightop = "";
+                    operation = "";
+                    textBox.Text = "";
+                }
+                // get operation
+                else
+                {
+                    if (rightop != "")
+                    {
+                        //Update_RightOp();
+                        leftop = rightop;
+                        rightop = "";
+                    }
+                    operation = s;
+                }
+            }
+        }
+
+        private void Update_RightOp()
+        {
+            int num1 = Int32.Parse(leftop);
+            int num2 = Int32.Parse(rightop);
+
+            Calculate calc = new Calculate();
+            rightop = (calc.PerformOperation(operation, num1, num2)).ToString();
+
         }
     }
 }
